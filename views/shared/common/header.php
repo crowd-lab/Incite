2315729c266e1262fbe5df98a8a9fe05101c5903
Nai-Ching Wang <naiching@vt.edu>
2015-11-17 17:13:28 -0500

    Added Demo for Timeline in transcribe.php

--- views/shared/common/header.php
+++ views/shared/common/header.php
@@ -1,5 +1,5 @@
 <?php
-queue_css_file(array('bootstrap', 'style', 'bootstrap.min', 'jquery.iviewer', 'bootstrap-multiselect', 'leaflet'));
+queue_css_file(array('bootstrap', 'style', 'bootstrap.min', 'jquery.iviewer', 'bootstrap-multiselect', 'leaflet', 'jquery.jqtimeline'));
 $db = get_db();
 ?>
 
@@ -19,6 +19,7 @@ $db = get_db();
     <?php echo js_tag('jquery.iviewer'); ?>
     <?php echo js_tag('bootstrap-multiselect'); ?>
     <?php echo js_tag('leaflet'); ?>
+    <?php echo js_tag('jquery.jqtimeline'); ?>
     <?php echo head_css(); ?>
 
 
--- views/shared/documents/connectid.php
+++ views/shared/documents/connectid.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-queue_css_file(array('bootstrap', 'style', 'bootstrap.min'));
-$db = get_db();
-
 include(dirname(__FILE__).'/../common/header.php');
 ?>
 
