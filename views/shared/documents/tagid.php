7f3422c7dfe7cc71d7186306faaee47cde02119d
Nai-Ching Wang <naiching@vt.edu>
2015-11-21 21:31:31 -0500

    Updated transcription layout in tagid.php

--- views/shared/documents/tagid.php
+++ views/shared/documents/tagid.php
@@ -19,11 +19,13 @@
     <div class="container">
         <div class="col-md-6" id="work-zone">
             <div style="position: fixed;" id="work-view">
+                <h4>Information:</h4>
                     <div>Title: <?php echo metadata($this->tag, array('Dublin Core', 'Title')); ?></div>
                     <div>Date: <?php echo metadata($this->tag, array('Dublin Core', 'Date')); ?></div>
                     <div>Location: <?php echo metadata($this->tag, array('Item Type Metadata', 'Location')); ?></div>
                     <div>Description: <?php echo metadata($this->tag, array('Dublin Core', 'Description')); ?></div>
-                <textarea name="transcribe_text" rows="20" id="transcribe_copy" style="width: 100%;"><?php print_r($this->transcription); ?></textarea>
+                <h4>Transcription:</h4>
+                <div style="border-style: solid;" name="transcribe_text" rows="20" id="transcribe_copy" style="width: 100%;"><?php print_r($this->transcription); ?></div>
                 <div class="wrapper">
                     <div id="document_img" class="viewer"></div>
                 </div>
