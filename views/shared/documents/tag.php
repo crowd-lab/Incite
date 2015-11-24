db5f1e06ed9329f1503672138796f325f8fdd906
Nai-Ching Wang <naiching@vt.edu>
2015-11-23 23:37:50 -0500

    Updated layout of tag

--- views/shared/documents/tag.php
+++ views/shared/documents/tag.php
@@ -34,44 +34,6 @@ include(dirname(__FILE__).'/../common/header.php');
         </div>
     </div>
 <?php endforeach; ?>
-
-        <div class="col-lg-4">
-             <form class="form-wrapper" >
-                    <input type="text" style="margin-bottom: 10px;" id="search1" placeholder="Keywords" required>
-            </form>
-            <form class="form-wrapper" >
-                    <input type="text" style="margin-bottom: 10px;" id="search1" placeholder="Locations" required>
-            </form>
-            <div class="two-col">
-                <div class="col1">
-                   <form class="form-wrapper" >
-                        <input type="text" id="company" placeholder="Date 1"/> 
-                   </form>
-                </div>
-              <div class="col2">
-                   <form class="form-wrapper" >
-                        <input type="text" id="company" placeholder="Date 2"/>
-                   </form>
-              </div>
-
-              <div style="margin-top: 5em;">
-                <p>Here's what other people are working on: </p>
-              </div>
-              <ul style="list-style-type:none">
-                <li>
-                    <p>1) User1 just found a diary from Emma LeConte in 1840! </p> 
-                </li>
-                <li>
-                    <p>2) User2 is decoding a mysterious historical document allegedly from South.  </p> 
-                </li>
-                <li>
-                    <p>3) User3 found a new document talking about Nationalism. </p> 
-                </li>
-                <li>
-                    <p>4) User4 is figuring out what the 3 documents have in common.</p> 
-                </li>
-              </ul>
-        </div>
             
 </div>
 
