0c52eeb6ca4acda0c595708cc69c87abb9a4d4e5
Nai-Ching Wang <naiching@vt.edu>
2015-11-28 18:51:29 -0500

    Updated DiscoverController

--- controllers/DiscoverController.php
+++ controllers/DiscoverController.php
@@ -18,9 +18,15 @@ class Incite_DiscoverController extends Omeka_Controller_AbstractActionControlle
     public function indexAction()
     {
 		if ($this->getRequest()->isGet()) {
+            //Process location search
+
+            //Process time search
+
+            //Process keyword search
+
+            //Go to the desired task based on the above result
 			if (isset($_GET['task'])) {
 				if ($_GET['task'] == "transcribe") {
-					//$this->_helper->viewRenderer('tagid');
 					$this->_redirect('/incite/documents/transcribe');
 				} else if ($_GET['task'] == "tag") {
 					$this->_redirect('/incite/documents/tag');
