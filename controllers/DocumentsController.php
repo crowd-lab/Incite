28f77471f1484a5be447576a604a247796829b03
Nai-Ching Wang <naiching@vt.edu>
2015-12-17 00:39:39 -0500

    Added: search(location,time) integrated with transcribe

--- controllers/DocumentsController.php
+++ controllers/DocumentsController.php
@@ -139,7 +139,7 @@ class Incite_DocumentsController extends Omeka_Controller_AbstractActionControll
             $current_page = 1;
             if (isset($_GET['page']))
                 $current_page = $_GET['page'];
-            $document_ids = array_slice(array_values(getDocumentsWithoutTranscription()), 0, 24);
+            $document_ids = array_slice(array_intersect(array_values(getDocumentsWithoutTranscription()), $_SESSION['Incite']['search']['time']['item_ids'], $_SESSION['Incite']['search']['location']['item_ids']), 0, 24);
             $max_records_to_show = 8;
             $total_pages = ceil(count($document_ids)/$max_records_to_show);
             $records_counter = 0;
@@ -289,7 +289,7 @@ class Incite_DocumentsController extends Omeka_Controller_AbstractActionControll
             $current_page = 1;
             if (isset($_GET['page']))
                 $current_page = $_GET['page'];
-            $document_ids = array_values(getDocumentsWithoutTag());
+            $document_ids = array_slice(array_intersect(array_values(getDocumentsWithoutTag()), $_SESSION['Incite']['search']['time']['item_ids'], $_SESSION['Incite']['search']['location']['item_ids']), 0, 24);
             $max_records_to_show = 8;
             $records_counter = 0;
             $records = array();
@@ -470,9 +470,10 @@ class Incite_DocumentsController extends Omeka_Controller_AbstractActionControll
                     }
                 }
             }
+            $document_ids = array_slice(array_intersect(array_values($connectable_documents), $_SESSION['Incite']['search']['time']['item_ids'], $_SESSION['Incite']['search']['location']['item_ids']), 0, 24);
             $records = array();
-            for ($i = 0; $i < count($connectable_documents); $i++) {
-                $records[] = $this->_helper->db->find($connectable_documents[$i]);
+            for ($i = 0; $i < count($document_ids); $i++) {
+                $records[] = $this->_helper->db->find($document_ids[$i]);
             }
 
             $current_page = 1;
