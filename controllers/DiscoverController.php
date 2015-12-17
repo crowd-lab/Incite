2b749611c6f8ba27f0cf3a9941eb3218f8d6351b
Nai-Ching Wang <naiching@vt.edu>
2015-12-16 22:47:12 -0500

    Added search(time) data are ready for processing

--- controllers/DiscoverController.php
+++ controllers/DiscoverController.php
@@ -9,6 +9,26 @@
  *
  * @package Incite 
  */
+require_once("DB_Connect.php");
+
+function getAllDocumentsBetweenDates($start, $end, $order='ASC')
+{
+
+    $db = DB_Connect::connectDB();
+    $item_ids = array();  
+    $stmt = $db->prepare("SELECT record_id FROM `omeka_element_texts` WHERE `element_id` = 40 AND `text` BETWEEN ? AND ? ORDER BY `text` ".$order);
+    $stmt->bind_param("ss", $start, $end);
+    $stmt->bind_result($item_id);
+    $stmt->execute();
+    while ($stmt->fetch()) {
+        $item_ids[] = $item_id;
+    }
+    $stmt->close();
+    $db->close();
+    
+    return $item_ids;
+}
+
 class Incite_DiscoverController extends Omeka_Controller_AbstractActionController
 {
     public function init()
@@ -18,9 +38,22 @@ class Incite_DiscoverController extends Omeka_Controller_AbstractActionControlle
     public function indexAction()
     {
 		if ($this->getRequest()->isGet()) {
+            //Assume there is session already because query is stored in session
+            $_SESSION['Incite']['search'] = array();
             //Process location search
 
             //Process time search
+            if (isset($_GET['time'])) {
+                $time_segs = explode(' - ', $_GET['time']);
+                if (count($time_segs) != 2) {
+                    echo 'wrong time format';
+                    die();
+                }
+                $start_time = $time_segs[0];
+                $end_time   = $time_segs[1];
+                $item_ids = getAllDocumentsBetweenDates($start_time, $end_time);
+                $_SESSION['Incite']['search']['time'] = array('query' => $_GET['time'], 'item_ids' => $item_ids);
+            }
 
             //Process keyword search
 
