83c49e934be7b69f1eed41c70b7180d41c535246
Nai-Ching Wang <naiching@vt.edu>
2015-12-18 21:52:17 -0500

    Updated create discussion layout

--- controllers/Incite_Questions_Table.php
+++ controllers/Incite_Questions_Table.php
@@ -127,7 +127,7 @@ function pullQuestionsForDocumentOnly($document_id)
 {
     $idArray = array();
     $db = DB_Connect::connectDB();
-    $stmt = $db->prepare("SELECT question_id FROM omeka_incite_documents_questions_conjunction INNER JOIN omeka_incite_questions ON omeka_incite_questions.id = omeka_incite_documents_questions_conjunction.question_id WHERE question_type = 0 AND document_id = ?");
+    $stmt = $db->prepare("SELECT question_id FROM omeka_incite_documents_questions_conjunction INNER JOIN omeka_incite_questions ON omeka_incite_questions.id = omeka_incite_documents_questions_conjunction.question_id WHERE question_type = 0 AND document_id = ? ORDER BY question_id");
     $stmt->bind_param("i", $document_id);
     $stmt->bind_result($question_id);
     $stmt->execute();
@@ -179,7 +179,7 @@ function getAllRepliesForQuestion($question_id)
 {
     $idArray = array();
     $db = DB_Connect::connectDB();
-    $stmt = $db->prepare("SELECT id FROM omeka_incite_replies WHERE question_id = ?");
+    $stmt = $db->prepare("SELECT id FROM omeka_incite_replies WHERE question_id = ? ORDER BY id");
     $stmt->bind_param("i", $question_id);
     $stmt->bind_result($id);
     $stmt->execute();
