12b1055f376848ab423903a7247022eb2c82e031
Nai-Ching Wang <naiching@vt.edu>
2016-01-11 20:47:51 -0500

    Refactored comments from transcribid

--- views/shared/documents/transcribeid.php
+++ views/shared/documents/transcribeid.php
@@ -6,150 +6,9 @@
     
     ?>
     <script type="text/javascript">
-
-        function getNewComments()
-        {
-            var documentId = <?php echo $this->transcription->id; ?>;
-            var request = $.ajax({
-                type: "POST",
-                url: "http://localhost/m4j/incite/ajax/getcommentsdoc",
-                data: {documentId: documentId},
-                success: function (data)
-                {
-                    var commentsArray = JSON.parse(data);
-
-
-                    for (var i = 0; i < commentsArray.length; i++)
-                    {
-
-                        var databaseDate = new Date(commentsArray[i]['question_timestamp']);
-
-
-                        var format = compareDates(databaseDate);
-                        var commentsArrayObject = {commentsArray};
-                        var isSignedIn = $.ajax({
-                            type: 'POST',
-                            url: 'http://localhost/m4j/incite/ajax/issignedin',
-                            async: false,
-                            data: {loopVar: i, commentArray: commentsArray, format: format},
-                            success: appendNewComment
-                        });
-                    }
-                }
-            });
-        }
-        function appendNewComment(dataArray)
-        {
-            
-            var parsedData = JSON.parse(dataArray);
-            var commentsArray = parsedData[2];
-            var format = parsedData[3];
-            var i = parseInt(parsedData[1]);
-            var boolean = parsedData[0];
-
-            var commentsArrayReplies = commentsArray[i]['question_replies'];
-            var commentsArrayRepliesTimestamp = commentsArray[i]['question_replies_timestamp'];
-            var commentsArrayRepliesUserData = commentsArray[i]['question_replies_user_data'];
-            if (boolean)
-            {
-                var dynamicLi = document.createElement('li');
-                dynamicLi.className = "cmmnt";
-                var dynamicDiv = document.createElement('div');
-                dynamicDiv.className = "cmmnt-content";
-                dynamicDiv.innerHTML = '<header><a href="javascript:void(0);" class="userlink">' + commentsArray[i]['user_info'][0] + '</a> - <span class="pubdate">' + format + '</span></header><p>' + commentsArray[i]['question_text'] + '</p>';
-
-                if (commentsArrayReplies != null && commentsArrayReplies.length > 0)
-                {
-                    var string = "<ul><li><header><a href='javascript:void(0);' class='userlink'>";
-                    for (var j = 0; j < commentsArrayReplies.length; j++)
-                    {
-                        var databaseDate = new Date(commentsArrayRepliesTimestamp[j]);
-                        string += commentsArrayRepliesUserData[j][0] + '</a> - <span class="pubdate">' + compareDates(databaseDate) + '</span></header><p>' + commentsArrayReplies[j] + "</p></li>";
-                        if (j != commentsArrayReplies.length - 1)
-                        {
-                            string += "<li><header><a href='javascript:void(0);' class='userlink'>";
-                        }
-                    }
-                    string += "</ul>"
-                    dynamicDiv.innerHTML += (string + '<button type="button" name="reply" class="btn btn-default reply-comment" id="reply' + i + '" value="' + commentsArray[i]['question_id'] + '">Reply</button>');
-                } else
-                {
-                    dynamicDiv.innerHTML += '<button type="button" name="reply" class="btn btn-default reply-comment" id="reply' + i + '" value="' + commentsArray[i]['question_id'] + '">Reply</button>';
-                }
-
-                dynamicLi.appendChild(dynamicDiv);
-                document.getElementById("comments").appendChild(dynamicLi);
-            } else
-            {
-                var dynamicLi = document.createElement('li');
-                dynamicLi.className = "cmmnt";
-                var dynamicDiv = document.createElement('div');
-                dynamicDiv.className = "cmmnt-content";
-                dynamicDiv.innerHTML = '<header><a href="javascript:void(0);" class="userlink">' + commentsArray[i]['user_info'][0] + '</a> - <span class="pubdate">' + format + '</span></header><p>' + commentsArray[i]['question_text'] + '</p>';
-                dynamicLi.appendChild(dynamicDiv);
-                document.getElementById("comments").appendChild(dynamicLi);
-            }
-        }
-        function compareDates(databaseDate)
-        {
-            var currentDate = new Date();
-            var differenceDate = Math.ceil((currentDate.getTime() - databaseDate.getTime()) / 1000);
-            var format = "posted ";
-            if (differenceDate < 60)
-            {
-                format += differenceDate + " second ago";
-            } else if (differenceDate < 3600)
-            {
-                format += (new Date).clearTime().addSeconds(differenceDate).toString('m') + " minutes ago";
-            } else if (differenceDate < 86400)
-            {
-                if (differenceDate < 7200)
-                {
-                    format += (new Date).clearTime().addSeconds(differenceDate).toString('H') + " hour ago";
-                } else
-                {
-                    format += (new Date).clearTime().addSeconds(differenceDate).toString('H') + " hours ago";
-                }
-            }
-            else if (differenceDate < 31540000)
-            {
-                if (differenceDate < 2629746)
-                {
-                    if (parseInt((new Date).clearTime().addSeconds(differenceDate).toString('d')) - 1 == (new Date).toString('d'))
-                    {
-                        format += "1 day ago";
-                    }
-                    else
-                    {
-                        format += (parseInt((new Date).clearTime().addSeconds(differenceDate).toString('d')) - parseInt((new Date).toString('d'))) + " days ago";
-                    }
-                } 
-                else
-                {
-                    format += (new Date).clearTime().addSeconds(differenceDate).toString('M') + " months ago";
-                }
-            } else
-            {
-                if (differenceDate < 63080000)
-                {
-                    format += " 1 year ago";
-                } else
-                {
-                    if ((new Date).clearTime().addSeconds(differenceDate).toString('YY').charAt(0) == '0')
-                    {
-                        format += (new Date).clearTime().addSeconds(differenceDate).toString('YY').charAt(1) + " year ago";
-                    } else
-                    {
-                        format += (new Date).clearTime().addSeconds(differenceDate).toString('YY') + " year ago";
-                    }
-                }
-            }
-            return format;
-        }
-
         $(function ()
         {
-            getNewComments();
+            getNewComments(<?php echo $this->transcription->id; ?>);
         });
     </script>
 
@@ -199,15 +58,12 @@
 
                 <div id="container">
                     <h3> Discussion </h3>
-                    <ul id="comments">
-
-                    </ul>
                     <div id="onLogin">
 <?php if (isset($_SESSION['Incite']['IS_LOGIN_VALID']) && $_SESSION['Incite']['IS_LOGIN_VALID'] == true /** && is_permitted * */): ?>
 
                             <form id="discuss-form" method="POST">
                                 <textarea name="transcribe_text" cols="60" rows="10" id="comment" placeholder="Your comment"></textarea>
-                                <button type="button" class="btn btn-default" onclick="submitComment()">Submit</button>
+                                <button type="button" class="btn btn-default" onclick="submitComment(<?php echo $this->transcription->id; ?>)">Submit</button>
                             </form>
 
 <?php else: ?>
@@ -215,6 +71,9 @@
 
                         <?php endif; ?>
                     </div>
+                    <ul id="comments">
+
+                    </ul>
                 </div>
             </div> 
         </div>
@@ -229,7 +88,7 @@
             {
                 if (event.target.name === "reply")
                 {
-                    var NewContent = '<form id="reply-form" method="POST"><textarea name="transcribe_text" cols="60" rows="10" id="replyBox' + event.target.id.substring(5) + '" placeholder="Your Reply"></textarea><button type="button" onclick="submitReply(event)" class="btn btn-default" id="submit' + event.target.id.substring(5) + '" value="' + event.target.value + '">Submit</button></form>';
+                    var NewContent = '<form id="reply-form" method="POST"><textarea name="transcribe_text" cols="60" rows="10" id="replyBox' + event.target.id.substring(5) + '" placeholder="Your Reply"></textarea><button type="button" onclick="submitReply(event<?php echo ', '.$this->transcription->id; ?>)" class="btn btn-default" id="submit' + event.target.id.substring(5) + '" value="' + event.target.value + '">Submit</button></form>';
                     $("#" + event.target.id).after(NewContent);
                     $("#" + event.target.id).remove();
                 }
@@ -251,49 +110,6 @@
         $('.viewer').height($(window).height() - $('.viewer')[0].getBoundingClientRect().top - 60);
         $('#transcribe_copy').height($(window).height() - $('.viewer')[0].getBoundingClientRect().top - 60);
 
-        function submitReply(event)
-        {
-            var replyText = document.getElementById('replyBox' + event.target.id.substring(6)).value;
-            var questionID = parseInt(event.target.value);
-            var documentId = <?php echo $this->transcription->id; ?>;
-            var request = $.ajax({
-                type: "POST",
-                url: "http://localhost/m4j/incite/ajax/postreply",
-                data: {replyText: replyText, originalQuestionId: questionID, documentId: documentId},
-                success: function (data)
-                {
-                    $("#comments").empty();
-                    getNewComments();
-                    notif({
-                       msg: "Successfully Posted Reply",
-                       type: "success",
-                       timeout: 2000
-                    });
-                    document.getElementById('replyBox' + event.target.id.substring(6)).value = "";
-                }
-            });
-        }
-        function submitComment()
-        {
-            var commentText = document.getElementById('comment').value;
-            var documentId = <?php echo $this->transcription->id; ?>;
-            var request = $.ajax({
-                type: "POST",
-                url: "http://localhost/m4j/incite/ajax/postcomment",
-                data: {documentId: documentId, commentText: commentText, type: 0},
-                success: function ()
-                {
-                    $("#comments").empty();
-                    getNewComments();
-                    notif({
-                       msg: "Successfully Posted Comment",
-                       type: "success",
-                       timeout: 2000
-                    });
-                    document.getElementById('comment').value = "";
-                }
-            })
-        }
 
     </script>
     <style>
---
+++ views/shared/javascripts/comments.js
@@ -0,0 +1,187 @@
+
+function getNewComments(documentId)
+{
+    //var documentId = <?php echo $this->transcription->id; ?>;
+    var request = $.ajax({
+        type: "POST",
+        url: "http://localhost/m4j/incite/ajax/getcommentsdoc",
+        data: {documentId: documentId},
+        success: function (data)
+        {
+            var commentsArray = JSON.parse(data);
+
+
+            for (var i = 0; i < commentsArray.length; i++)
+            {
+
+                var databaseDate = new Date(commentsArray[i]['question_timestamp']);
+
+
+                var format = compareDates(databaseDate);
+                var commentsArrayObject = {commentsArray};
+                var isSignedIn = $.ajax({
+                    type: 'POST',
+                    url: 'http://localhost/m4j/incite/ajax/issignedin',
+                    async: false,
+                    data: {loopVar: i, commentArray: commentsArray, format: format},
+                    success: appendNewComment
+                });
+            }
+        }
+    });
+}
+function appendNewComment(dataArray)
+{
+    
+    var parsedData = JSON.parse(dataArray);
+    var commentsArray = parsedData[2];
+    var format = parsedData[3];
+    var i = parseInt(parsedData[1]);
+    var boolean = parsedData[0];
+
+    var commentsArrayReplies = commentsArray[i]['question_replies'];
+    var commentsArrayRepliesTimestamp = commentsArray[i]['question_replies_timestamp'];
+    var commentsArrayRepliesUserData = commentsArray[i]['question_replies_user_data'];
+    if (boolean)
+    {
+        var dynamicLi = document.createElement('li');
+        dynamicLi.className = "cmmnt";
+        var dynamicDiv = document.createElement('div');
+        dynamicDiv.className = "cmmnt-content";
+        dynamicDiv.innerHTML = '<header><a href="javascript:void(0);" class="userlink">' + commentsArray[i]['user_info'][0] + '</a> - <span class="pubdate">' + format + '</span></header><p>' + commentsArray[i]['question_text'] + '</p>';
+
+        if (commentsArrayReplies != null && commentsArrayReplies.length > 0)
+        {
+            var string = "<ul><li><header><a href='javascript:void(0);' class='userlink'>";
+            for (var j = 0; j < commentsArrayReplies.length; j++)
+            {
+                var databaseDate = new Date(commentsArrayRepliesTimestamp[j]);
+                string += commentsArrayRepliesUserData[j][0] + '</a> - <span class="pubdate">' + compareDates(databaseDate) + '</span></header><p>' + commentsArrayReplies[j] + "</p></li>";
+                if (j != commentsArrayReplies.length - 1)
+                {
+                    string += "<li><header><a href='javascript:void(0);' class='userlink'>";
+                }
+            }
+            string += "</ul>"
+            dynamicDiv.innerHTML += (string + '<button type="button" name="reply" class="btn btn-default reply-comment" id="reply' + i + '" value="' + commentsArray[i]['question_id'] + '">Reply</button>');
+        } else
+        {
+            dynamicDiv.innerHTML += '<button type="button" name="reply" class="btn btn-default reply-comment" id="reply' + i + '" value="' + commentsArray[i]['question_id'] + '">Reply</button>';
+        }
+
+        dynamicLi.appendChild(dynamicDiv);
+        document.getElementById("comments").appendChild(dynamicLi);
+    } else
+    {
+        var dynamicLi = document.createElement('li');
+        dynamicLi.className = "cmmnt";
+        var dynamicDiv = document.createElement('div');
+        dynamicDiv.className = "cmmnt-content";
+        dynamicDiv.innerHTML = '<header><a href="javascript:void(0);" class="userlink">' + commentsArray[i]['user_info'][0] + '</a> - <span class="pubdate">' + format + '</span></header><p>' + commentsArray[i]['question_text'] + '</p>';
+        dynamicLi.appendChild(dynamicDiv);
+        document.getElementById("comments").appendChild(dynamicLi);
+    }
+}
+function compareDates(databaseDate)
+{
+    var currentDate = new Date();
+    var differenceDate = Math.ceil((currentDate.getTime() - databaseDate.getTime()) / 1000);
+    var format = "posted ";
+    if (differenceDate < 60)
+    {
+        format += differenceDate + " second ago";
+    } else if (differenceDate < 3600)
+    {
+        format += (new Date).clearTime().addSeconds(differenceDate).toString('m') + " minutes ago";
+    } else if (differenceDate < 86400)
+    {
+        if (differenceDate < 7200)
+        {
+            format += (new Date).clearTime().addSeconds(differenceDate).toString('H') + " hour ago";
+        } else
+        {
+            format += (new Date).clearTime().addSeconds(differenceDate).toString('H') + " hours ago";
+        }
+    }
+    else if (differenceDate < 31540000)
+    {
+        if (differenceDate < 2629746)
+        {
+            if (parseInt((new Date).clearTime().addSeconds(differenceDate).toString('d')) - 1 == (new Date).toString('d'))
+            {
+                format += "1 day ago";
+            }
+            else
+            {
+                format += (parseInt((new Date).clearTime().addSeconds(differenceDate).toString('d')) - parseInt((new Date).toString('d'))) + " days ago";
+            }
+        } 
+        else
+        {
+            format += (new Date).clearTime().addSeconds(differenceDate).toString('M') + " months ago";
+        }
+    } else
+    {
+        if (differenceDate < 63080000)
+        {
+            format += " 1 year ago";
+        } else
+        {
+            if ((new Date).clearTime().addSeconds(differenceDate).toString('YY').charAt(0) == '0')
+            {
+                format += (new Date).clearTime().addSeconds(differenceDate).toString('YY').charAt(1) + " year ago";
+            } else
+            {
+                format += (new Date).clearTime().addSeconds(differenceDate).toString('YY') + " year ago";
+            }
+        }
+    }
+    return format;
+}
+
+
+function submitReply(event, documentId)
+{
+    var replyText = document.getElementById('replyBox' + event.target.id.substring(6)).value;
+    var questionID = parseInt(event.target.value);
+    //var documentId = <?php echo $this->transcription->id; ?>;
+    var request = $.ajax({
+        type: "POST",
+        url: "http://localhost/m4j/incite/ajax/postreply",
+        data: {replyText: replyText, originalQuestionId: questionID, documentId: documentId},
+        success: function (data)
+        {
+            $("#comments").empty();
+            //getNewComments(<?php echo $this->transcription->id; ?>);
+            getNewComments(documentId);
+            notif({
+               msg: "Successfully Posted Reply",
+               type: "success",
+               timeout: 2000
+            });
+            document.getElementById('replyBox' + event.target.id.substring(6)).value = "";
+        }
+    });
+}
+function submitComment(documentId)
+{
+    var commentText = document.getElementById('comment').value;
+    //var documentId = <?php echo $this->transcription->id; ?>;
+    var request = $.ajax({
+        type: "POST",
+        url: "http://localhost/m4j/incite/ajax/postcomment",
+        data: {documentId: documentId, commentText: commentText, type: 0},
+        success: function ()
+        {
+            $("#comments").empty();
+            //getNewComments(<?php echo $this->transcription->id; ?>);
+            getNewComments(documentId);
+            notif({
+               msg: "Successfully Posted Comment",
+               type: "success",
+               timeout: 2000
+            });
+            document.getElementById('comment').value = "";
+        }
+    })
+}
