367dc842ddcae5f002a26eae04c810fa406040ec
Nai-Ching Wang <naiching@vt.edu>
2016-01-23 23:51:02 -0500

    Added enviroment settings

---
+++ controllers/Incite_Env_Setting.php
@@ -0,0 +1,15 @@
+<?php
+//Redirection
+define('REDIRECTOR_URL', 'incite/documents/redirect');
+
+
+//Tag Task
+$categories = array('ORGANIZATION', 'PERSON', 'LOCATION', 'EVENT');
+$category_colors = array('ORGANIZATION' => 'red', 'PERSON' => 'orange', 'LOCATION' => 'yellow', 'EVENT' => 'gray');
+
+
+
+//Connect
+define("MINIMUM_COMMON_TAGS_FOR_RELATED_DOCUMENTS", 2);
+
+?>
