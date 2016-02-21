1377286a79cf5236dfc9c18da6d66ab906519511
Nai-Ching Wang <naiching@vt.edu>
2016-02-20 22:18:47 -0500

    Added incite-instructions

---
+++ incite-instructions/index.php
@@ -0,0 +1,38 @@
+
+
+
+<html>
+
+
+	<body>
+	====
+		Latest Version of User Instructions for Incite: 02/01/2016 (<a href="User instructions - 2016-02-01.docx">Download</a>)
+	====
+
+		<br>
+		<br>
+		<br>
+		<br>
+		==========Coming Features (expected to be deployed on 03/01/2016)==========
+		<br>
+		* Support multiple images for one document <br>
+		* New improved work viewer for transcribe, tag, and connect<br>
+		* New improved related document viewer for discuss<br>
+		* Tag: Auto-sliding table when there are too many tags<br>
+		* Progress bar for each document (based on the progress of transcribing, tagging, and connecting)<br>
+		* User activity history<br>
+		* Group information<br>
+		* New notification system<br>
+		* Various UI tweaks<br>
+		<br>
+		<br>
+		<br>
+		===========Update History==========
+		<br>
+		====02/01/2016====<br>
+		* First public release
+		
+	</body>
+
+
+</html>
