3be1d71d81358a4aaa5110cdec0ed982c2822b08
holasojo <joso930115@gmail.com>
2016-07-01 10:07:12 -0400

    Remove the CS@VT logo and the VT logo and add the Center for HCI logo. Make the footer logos a bit bigger so the text is readable.

--- views/shared/common/footer.php
+++ views/shared/common/footer.php
@@ -2,10 +2,8 @@
 
 <div id="footer" style="margin-left: 150px; margin-right: 150px; margin-top: 80px; border-top: 1px solid; border-top-color: #B2B1B1; padding: 25px; text-align: center;">
     Sponsors:
-    <a href="http://www.civilwar.vt.edu/" target="_blank"><img style="max-height: 80px; margin-left: 20px; margin-right: 20px;" src="<?php echo getFullOmekaUrl(); ?>/plugins/Incite/views/shared/images/vccw_logo.jpg"></a>
-    <a href="http://www.archives.gov/nhprc/" target="_blank"><img style="max-height: 80px; margin-left: 20px; margin-right: 20px;" src="<?php echo getFullOmekaUrl(); ?>/plugins/Incite/views/shared/images/nhprc_logo.jpg"></a>
-    <a href="http://www.vt.edu/" target="_blank"><img style="max-height: 80px; margin-left: 20px; margin-right: 20px;" src="<?php echo getFullOmekaUrl(); ?>/plugins/Incite/views/shared/images/vt_logo.png"></a>
-    <a href="http://www.cs.vt.edu/" target="_blank"><img style="max-height: 80px; margin-left: 20px; margin-right: 20px;" src="<?php echo getFullOmekaUrl(); ?>/plugins/Incite/views/shared/images/vt_cs_logo.png"></a>
-
+    <a href="http://www.civilwar.vt.edu/" target="_blank"><img style="max-height: 110px; margin-left: 25px; margin-right: 25px;" src="<?php echo getFullOmekaUrl(); ?>/plugins/Incite/views/shared/images/vccw_logo.jpg"></a>
+    <a href="http://www.archives.gov/nhprc/" target="_blank"><img style="max-height: 130px; margin-left: 25px; margin-right: 25px;" src="<?php echo getFullOmekaUrl(); ?>/plugins/Incite/views/shared/images/nhprc_logo.jpg"></a>
+    <a href="http://www.hci.vt.edu/" target="_blank"><img style="max-height: 70px; margin-left: 25px; margin-right: 25px;" src="<?php echo getFullOmekaUrl(); ?>/plugins/Incite/views/shared/images/centerforhci_logo.png"></a>
 
 </div>
