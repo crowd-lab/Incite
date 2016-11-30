5f6c7006bd0cc9f16631d55d8f6052e446ad22f2
Nai-Ching Wang <naiching@vt.edu>
2016-11-30 13:55:34 -0500

    Added NASA TLX to postsurvey

---
+++ views/shared/documents/postsurveytlx.php
@@ -0,0 +1,68 @@
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Section 2</h3>
+                </div>
+                <div style="padding: 15px;">
+                    <div class="form-group">
+                        <div><label>How mentally demanding was the task?</label></div>
+                        <label class="radio-inline"><input type="radio" name="tlx_men" value="1">1 (very low)</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_men" value="2">2</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_men" value="3">3</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_men" value="4">4</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_men" value="5">5</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_men" value="6">6</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_men" value="7">7 (very high)</label>
+                    </div>
+                    <div class="form-group">
+                        <div><label>How physically demanding was the task?</label></div>
+                        <label class="radio-inline"><input type="radio" name="tlx_phy" value="1">1 (very low)</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_phy" value="2">2</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_phy" value="3">3</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_phy" value="4">4</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_phy" value="5">5</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_phy" value="6">6</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_phy" value="7">7 (very high)</label>
+                    </div>
+                    <div class="form-group">
+                        <div><label>How hurried or rushed was the pace of the task?</label></div>
+                        <label class="radio-inline"><input type="radio" name="tlx_tem" value="1">1 (very low)</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_tem" value="2">2</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_tem" value="3">3</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_tem" value="4">4</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_tem" value="5">5</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_tem" value="6">6</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_tem" value="7">7 (very high)</label>
+                    </div>
+                    <div class="form-group">
+                        <div><label>How successful were you in accomplishing what you were asked to do?</label></div>
+                        <label class="radio-inline"><input type="radio" name="tlx_per" value="1">1 (very low)</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_per" value="2">2</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_per" value="3">3</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_per" value="4">4</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_per" value="5">5</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_per" value="6">6</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_per" value="7">7 (very high)</label>
+                    </div>
+                    <div class="form-group">
+                        <div><label>How hard did you have to work to accomplish your level of performance?</label></div>
+                        <label class="radio-inline"><input type="radio" name="tlx_eff" value="1">1 (very low)</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_eff" value="2">2</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_eff" value="3">3</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_eff" value="4">4</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_eff" value="5">5</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_eff" value="6">6</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_eff" value="7">7 (very high)</label>
+                    </div>
+                    <div class="form-group">
+                        <div><label>How insecure, discouraged, irritated, stressed, and annoyed were you?</label></div>
+                        <label class="radio-inline"><input type="radio" name="tlx_fru" value="1">1 (very low)</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_fru" value="2">2</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_fru" value="3">3</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_fru" value="4">4</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_fru" value="5">5</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_fru" value="6">6</label>
+                        <label class="radio-inline"><input type="radio" name="tlx_fru" value="7">7 (very high)</label>
+                    </div>
+                </div>
+                <div style="clearfix"></div>
+            </div> 
