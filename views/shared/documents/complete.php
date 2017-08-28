7e5942487de803e17c8fd2330f0e92caa5e4dccd
Nai-Ching Wang <naiching@vt.edu>
2017-08-28 13:18:24 -0400

    Updated complete page for submission to AMT

--- views/shared/documents/complete.php
+++ views/shared/documents/complete.php
@@ -3,11 +3,30 @@
 <?php
 $task = "transcribe";
 include(dirname(__FILE__).'/../common/header.php');
+
+$mturk_url = "";
+//$endpoint = "production";
+$endpoint = "sandbox";
+if ($endpoint == "production")
+    $mturk_url = "https://www.mturk.com/mturk/externalSubmit";
+else
+    $mturk_url = "https://workersandbox.mturk.com/mturk/externalSubmit";
+
+
 ?>
 
 
+<div style="text-align: center">
+<h2>You have completed the study.</h2>
+<h2>Please click the following button to finish the HIT.</h2>
+<h2>Thank you for your participation.</h2>
+</div>
+<br>
+<br>
 
-<h2>You have completed the study. Thank you for your participation.</h2>
+<form method="post" action="<?php echo $mturk_url; ?>">
+<div style="text-align: center"><button style="width: 100px;" class="btn btn-primary">Finish</button></div>
+</form>
 
 <script>
 $(function() {
