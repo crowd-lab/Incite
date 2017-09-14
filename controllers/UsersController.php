1fcd3ea7a3fa6ae42c31e2da07cd787617c3bf73
Nai-Ching Wang <naiching@vt.edu>
2017-09-14 14:07:36 -0400

    Added auto-grading for tone, tags and theme

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -147,6 +147,102 @@ class Incite_UsersController extends Omeka_Controller_AbstractActionController {
         die();
 
     }
+    public function dumpstudydata3Action() { //grading
+        $separator = "^";
+        $db = DB_Connect::connectDB();
+        $stmt = $db->prepare("SELECT id, technique, pretest_response, summarytone_baseline, summarytone_condition, summarytone_revised, tag_baseline, tag_condition, tag_revised, connect_baseline, connect_condition, connect_revised, posttest_response, postsurvey_response FROM `study22` WHERE is_completed = 2 AND id > 5");
+        $stmt->bind_result($id, $technique, $pre_response, $summarytone_baseline, $summarytone_condition, $summarytone_revised, $tag_baseline, $tag_condition, $tag_revised, $connect_baseline, $connect_condition, $connect_revised, $posttest_response, $postsurvey_response);
+        $stmt->execute();
+        echo 'id'.$separator.'technique'.$separator.'pretest_response'.$separator.'summarytone_baseline'.$separator.'summarytone_condition'.$separator.'summarytone_revised'.$separator.'tag_baseline'.$separator.'tag_condition'.$separator.'tag_revised'.$separator.'connect_baseline'.$separator.'connect_condition'.$separator.'connect_revised'.$separator.'posttest_response'.$separator.'postsurvey_response'."\n";
+        while ($stmt->fetch()) {
+            unset($baseline);
+            if ($technique == "scim") {
+                $baseline = json_decode($summarytone_baseline, true);
+                $revised = json_decode($summarytone_revised, true);
+            }
+            if (isset($baseline) && $baseline["response"]['summary'] != $revised['response']['summary']) {
+                echo "\n\nDiff:".(strlen($revised['response']['summary'])-strlen($baseline['response']['summary']))."\n\n";
+            }
+            echo $id.$separator.$technique.$separator.'"'.$pre_response.'"'.$separator.'"'.$summarytone_baseline.'"'.$separator.'"'.$summarytone_condition.'"'.$separator.'"'.$summarytone_revised.'"'.$separator.'"'.$tag_baseline.'"'.$separator.'"'.$tag_condition.'"'.$separator.'"'.$tag_revised.'"'.$separator.'"'.$connect_baseline.'"'.$separator.'"'.$connect_condition.'"'.$separator.'"'.$connect_revised.'"'.$separator.'"'.$posttest_response.'"'.$separator.'"'.$postsurvey_response."'\n";
+            //echo $technique.$separator.$pre_response.$separator."'".$summarytone_baseline."'".$separator.$summarytone_condition.$separator.$summarytone_revised.$separator.$tag_baseline.$separator.$tag_condition.$separator.$tag_revised.$separator.$connect_baseline.$separator.$connect_condition.$separator.$connect_revised.$separator.$posttest_response.$separator.$postsurvey_response."\n";
+        }
+        $stmt->close();
+        $db->close();
+        die();
+
+    }
+
+    function check_tone_rating($user, $gold = null) {
+        if ($gold == null) {
+            $gold = array("tone1" => 0, "tone2" => 0, "tone3" => 0, "tone4" => 0, "tone5" => 0, "tone6" => 0);
+        }
+        $user = json_decode($user, true);
+        $total_abs_diff = 0;
+        $total_diff = 0;
+        for ($i = 1; $i <= 6; $i++) {
+            $diff = $user['tone'.$i] - $gold["tone".$i];
+            $total_diff += $diff;
+            $total_abs_diff += abs($diff);
+        }
+        return array('abs_diff' => $total_abs_diff, 'diff' => $total_diff);
+    }
+    function check_tag($user, $gold = null) { // currently only tag names and their catorigies
+        if ($gold == null) {
+            $gold = array("Bedford" => 1, "Bedford2" => 1, "Maylans Regiment" => 4, "Washington" => 3, "New Windsor" => 1, "Gen. Washington" => 3); //added Gen. Washington as an alias for Washington. This doesn't affect $total since it's an alias.
+        }
+        $user = json_decode($user, true);
+        $seen_bedford = false;
+        $total = 5; // total # of tags in gold so $misses = $total - $(xxx)_hits
+        $tag_hits = 0;
+        $cat_hits = 0;
+        $extras = 0;
+        foreach ($user as $tag) {
+            //ignore non-Location(1), non-Person(3), non-Organization(4) for now
+            if ($tag['category'] != 1 && $tag['category'] != 3 && $tag['category'] != 4) {
+                continue;
+            }
+            //two if's for dealing with multiple Bedford tags
+            if ($seen_bedford && $tag['entity'] == 'Bedford') {
+                $tag['entity'] = 'Bedford2';
+            }
+            if (!$seen_bedford && $tag['entity'] == 'Bedford') {
+                $seen_bedford = true;
+            }
+            //actual grading
+            if (array_key_exists($tag['entity'], $gold)) { //tag matches a tag in gold
+                $tag_hits++;
+                if ($tag['category'] == $gold[$tag['entity']]) { //cat matches as well
+                    $cat_hits++;
+                }
+            } else { //tag doesn't match any tag in gold
+                $extras++;
+            }
+        }
+        $tag_misses = $total - $tag_hits;
+        $cat_misses = $total - $cat_misses;
+        $precision_tag = (float)$tag_hits/($tag_hits+$extras);
+        $precision_cat = (float)$tag_cat/($tag_hits+$extras);
+        $recall_tag = (float)$tag_hits/$total;
+        $recall_cat = (float)$cat_hits/$total;
+
+        return array('tag' => array('precision' => $precision_tag, 'recall' => $recall_tag, 'hit' => $tag_hits, 'miss' => $tag_misses, 'total' => $total),
+                     'cat' => array('precision' => $precision_cat, 'recall' => $recall_cat, 'hit' => $cat_hits, 'miss' => $cat_misses, 'total' => $total));
+    }
+    function check_theme_rating($user, $gold = null) {
+        if ($gold == null) {
+            $gold = array("subject1" => 0, "subject2" => 0, "subject3" => 0, "subject4" => 0, "subject5" => 0, "subject6" => 0, "subject7" => 0, "subject8" => 0, "subject9" => 0, "subject10" => 0, "subject11" => 0, "subject12" => 0, "subject13" => 0, "subject14" => 0);
+        }
+        $user = json_decode($user, true);
+        $total_abs_diff = 0;
+        $total_diff = 0;
+        for ($i = 1; $i <= 14; $i++) {
+            $diff = $user['subject'.$i] - $gold["subject".$i];
+            $total_diff += $diff;
+            $total_abs_diff += abs($diff);
+        }
+        return array('abs_diff' => $total_abs_diff, 'diff' => $total_diff);
+    }
+
 
 
 /**
